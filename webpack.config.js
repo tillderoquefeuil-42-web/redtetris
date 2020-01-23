57c2689747a06f362af564eec29c16a478d9c050
till de roquefeuil <tillderoquefeuil@gmail.com>
2020-01-23 16:05:25 +0100

    2 low vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: './src/client/index.js',
-
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   },
+  mode: 'production',
+
+  watchOptions: {
+    poll: 1000,
+    aggregateTimeout:300,
+  },
+
+  performance: { hints: false },
 
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query:{
         presets: ["es2015", "react", "stage-0"]
       }
-    }]
-  }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name][hash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
